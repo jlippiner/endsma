6766b4e2b1bd884ad7b0c246a3a4f36555c50b9f
joshua.lippiner <josh@helpishere.org>
2009-03-14 15:09:06 -0400

    Updated report output

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
   layout "reports"
 
   def index
-    @output = Clicks.find(:all, :order => "created_at")
+    @output = Clicks.find(:all, :order => "created_at DESC")
     @groups = Clicks.find_by_sql("SELECT referral, tags, count(*) AS Visits FROM clicks GROUP BY referral, tags ORDER BY count(*) DESC")
   end
 
