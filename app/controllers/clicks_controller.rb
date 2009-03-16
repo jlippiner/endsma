3b4b4bd3d2d82939c7f6f5bbc12cbba9a13c7fae
joshua.lippiner <josh@helpishere.org>
2009-03-16 18:42:03 -0400

    Updated redirect link

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -3,7 +3,7 @@ class ClicksController < ApplicationController
 
   def new
     if Clicks.create(:referral => params[:ref], :tags => params[:tags], :ip_address => request.remote_ip, :useragent => request.env['HTTP_USER_AGENT'] )
-      redirect_to "http://www.petitiontocuresma.com"
+      redirect_to "http://www.thepetitionsite.com/182/petition-to-cure-SMA"
     end
   end
   
